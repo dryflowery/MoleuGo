ba67eba182e589f78341892ee5d02f3dfee893f9
pwk0131 <pakr051323@gmail.com>
2024-11-09 21:42:08 +0900

    delete release note

--- MoleuGo/src/pages/mainPage.svelte
+++ MoleuGo/src/pages/mainPage.svelte
@@ -1,36 +1,31 @@
-<!--메인페이지-->
 <script>
     import Footer from "../component/Footer.svelte";
     import Header from "../component/revised_version/Header_2.svelte";
-    import {isListVisible } from "../lib/store"
-    import {isLogin } from "../lib/store"
+    import { isListVisible } from "../lib/store";
+    import { isLogin } from "../lib/store";
     import AuthPage from "../component/isAuthpage/authPage.svelte";
     import NoAuthPage from "../component/isAuthpage/noAuthPage.svelte";
     import AlgorithmList from "../component/AlgorithmList.svelte";
-
-</script>
+  </script>
   
-<main>
-    <!-- HTML 구조는 header-container, main-container, footer-container으로 구성하기(Header, Footer 없는 페이지 제외) -->
-    <!--  해당 클래스의 css는 app.css에 정의 -->
+  <main>
     <div class="header-container">
-        <Header/>
+      <Header />
     </div>
-
-    <div class="main-container" style="transform: translateX({$isListVisible ? 400 : 0}px); " >
-        <AuthPage/>
-    </div>
-
-    <div class="footer-container">
-        <Footer/>
-    </div>
- </main>
- 
   
-<style>
+    <div class="main-container" style="transform: translateX({$isListVisible ? 400 : 0}px); margin-right: {$isListVisible ? 400 : 0}px;">
+      <AuthPage />
+    </div>
+  
+    <div class="footer-container">
+      <Footer />
+    </div>
+  </main>
+  
+  <style>
     .main-container {
-        color: white;
-        transition: transform 0.5s ease;
+      color: white;
+      transition: transform 0.5s ease, margin 0.5s ease;
     }
-
-</style>
+  </style>
+  
