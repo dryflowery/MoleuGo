89226a4fc66c16ba2056634c10d8af897d924710
Deokhyeon Kim <skywum@naver.com>
2024-11-12 00:17:45 +0900

    페이지 연결

--- MoleuGo/src/App.svelte
+++ MoleuGo/src/App.svelte
@@ -3,11 +3,13 @@
   import Index from "./routes/Index.svelte";
   import MainPage from './routes/MainPage.svelte';
   import SignupPage from './routes/SignupPage.svelte';
+  import FindPassword from './routes/FindPassword.svelte'
 
   const routes = {
     '/': Index,
     '/main' : MainPage,
-    '/signup': SignupPage
+    '/signup': SignupPage,
+    '/findpassword' : FindPassword
   };
 </script>
 
