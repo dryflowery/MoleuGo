f103aff79032ae5db584a5f5c52120e19a3a84b9
dryflowery <sechane12@naver.com>
2025-05-16 05:08:07 +0900

    guide-book demo

--- frontend/src/component/guide/bubblesort/sheet/BubbleSortSheet1.svelte
+++ frontend/src/component/guide/bubblesort/sheet/BubbleSortSheet1.svelte
@@ -1,5 +1,15 @@
 <main class="sheet">
-    <header class="page-title">첫 번째 페이지 제목</header>
-    <section class="page-content">첫 번째 페이지 본문</section>
+    <header class="page-title">버블 정렬이란?</header>
+
+    <section class="page-content">
+        <div class="page-content-img">
+            <img src="assets/guide/bubblesort/1.png">
+        </div>
+
+        <div class="page-content-txt">
+            버블 정렬(Bubble Sort)은 가장 단순한 정렬 알고리즘 중 하나로, 인접한 두 값을 비교하여 큰 값을 뒤로 보내는 방식으로 작동합니다.<br><br>
+            마치 거품이 물 위로 떠오르듯, 가장 큰 값이 리스트의 끝으로 이동하기 때문에 '버블'이라는 이름이 붙었습니다.
+        </div>
+    </section>
     <footer class="page-num">1</footer>
 </main>
--- frontend/src/component/guide/bubblesort/sheet/BubbleSortSheet2.svelte
+++ frontend/src/component/guide/bubblesort/sheet/BubbleSortSheet2.svelte
@@ -1,5 +1,16 @@
 <main class="sheet">
-    <header class="page-title">두 번째 페이지 제목</header>
-    <section class="page-content">두 번째 페이지 본문</section>
+    <header class="page-title">버블 정렬의 작동 원리(오름차순)</header>
+
+    <section class="page-content">
+        <div class="page-content-img">
+            <img src="assets/guide/bubblesort/2.png">
+        </div>
+
+        <div class="page-content-txt">
+            버블 정렬은 리스트를 반복적으로 순회하면서 인접한 두 요소를 비교하여, 왼쪽 값이 더 클 경우 두 값을 서로 교환합니다.<br><br>
+            이 과정을 통해 각 반복마다 가장 큰 값이 리스트의 오른쪽 끝으로 이동하게 되며, 배열이 완전히 정렬될 때까지 반복됩니다.
+        </div>
+    </section>
+
     <footer class="page-num">2</footer>
 </main>
