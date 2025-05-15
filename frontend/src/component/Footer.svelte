a5236afd4584bddb1a0ef510c46a4f8cf1154a28
dryflowery <sechane12@naver.com>
2025-05-16 03:29:14 +0900

    add footer element

--- frontend/src/component/Footer.svelte
+++ frontend/src/component/Footer.svelte
@@ -1,15 +1,37 @@
 <script>
 
 </script>
-  
+
 <main>
-   
+    <div id="footer-container">
+        <div id="footer-github">
+            <ion-icon name="logo-github" style="font-size: 1.5rem" class="footer-element"></ion-icon>
+        </div>
+        <div id="footer-teams" class="footer-element">Teams</div>
+        <div id="footer-about" class="footer-element">About</div>
+    </div>
 </main>
-  
+
 <style>
-   main {
+    main {
         height: 39px;
         border-top: 1px solid #3d444d;
-        background-color: #000000; 
+        background-color: #000000;
+        color: white;
+        overflow: hidden;
+        font-size: 0.8rem;
+    }
+
+    #footer-container {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        margin-bottom: 25px;
+        justify-content: center;
+    }
+
+    .footer-element:hover {
+        cursor: pointer;
+        text-decoration: underline;
     }
 </style>
